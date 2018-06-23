8ed4073556201aa048e8fc3cf844dddc69e51268
Juan Pablo Senia <juans.200@hotmail.com>
2018-06-23 12:53:19 -0300

    ControladorDeCamara

--- Practico Motores Graficos/Assets/Scripts/CameraController.cs
+++ Practico Motores Graficos/Assets/Scripts/CameraController.cs
@@ -7,13 +7,39 @@ public class CameraController : MonoBehaviour {
 	Vector3 ogPos;
 
 	public Transform target;
+    public Transform cam;
+    public Quaternion camRotation;
 	public float camSpeed;
 
-	void Start () {
+    public bool isOnBuilding;
+
+
+    void Start () {
 		ogPos = transform.position;
-	}
+        camRotation = cam.rotation;
+    }
 	
-	void FixedUpdate () {
-		transform.position = Vector3.Lerp (transform.position, target.position + (target.forward * 10) + ogPos, camSpeed);
+	void FixedUpdate ()
+    {
+        transform.position = Vector3.Lerp (transform.position, target.position + (target.forward * 10) + ogPos, camSpeed);
+        if (isOnBuilding)
+        {
+            cam.rotation = Quaternion.Lerp(cam.rotation, Quaternion.Euler(81, 0, 0), 5 * Time.deltaTime);
+            cam.transform.position = Vector3.Lerp(cam.transform.position, new Vector3(cam.transform.position.x, cam.transform.position.y, target.transform.position.z), 5*Time.deltaTime);
+        }
+        else
+        {
+            cam.rotation = Quaternion.Lerp(cam.rotation, camRotation, 5*Time.deltaTime);
+            cam.transform.position = Vector3.Lerp(cam.transform.position, new Vector3(cam.transform.position.x, cam.transform.position.y, transform.position.z), 5 * Time.deltaTime);
+        }
+        isOnBuilding = false;
 	}
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "CollisionDmg")
+        {
+            isOnBuilding = true;
+        }
+    }
 }
